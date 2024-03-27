083f81e83844fd08eee69484b694b9fce2c0f540
hachadorian <hachadorian98@gmail.com>
2024-03-27 17:10:48 -0400

    philadelphia comps

---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  props: { text: String },
+};
+</script>
+<template>
+  <button class="pvm-search-control-button">{{ text }}</button>
+</template>
+<style>
+.pvm-search-control-button {
+  display: inline-block;
+  color: #fff;
+  background: #0f4d90;
+  margin-right: 1.5px;
+  height: 50px;
+}
+
+button {
+  padding: 0;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  border: 0;
+  border-radius: 0;
+  background: transparent;
+  line-height: 1;
+  cursor: auto;
+}
+
+button:focus {
+  outline: none;
+}
+</style>
---
+++ src/components/TextField.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  props: {
+    placeholder: String,
+  },
+};
+</script>
+<template>
+  <input class="pvm-search-control-input" :placeholder="placeholder" />
+</template>
+<style>
+.pvm-search-control-input {
+  display: inline-block;
+  border: 0;
+  padding: 15px;
+  font-family: Montserrat, Tahoma, sans-serif;
+  font-size: 16px;
+  width: 250px;
+  height: 50px;
+  border-style: solid;
+  border-color: #0f4d90;
+  border-width: 2px;
+  outline: none;
+}
+</style>
