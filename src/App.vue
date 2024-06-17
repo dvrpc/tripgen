f71de7513f7b04a95f6a3fdcc7dd8603542f937b
hachadorian <hachadorian98@gmail.com>
2024-06-17 12:59:06 -0400

    branding

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,21 @@ const location = {
   lat: 42.213995,
   bearing: 0,
   pitch: 0,
-  zoom: 9,
+  zoom: 10,
 };
 </script>
 
 <template>
+  <header>
+    <div id="branding">
+      <img id="dvrpc-logo" src="./assets/dvrpc.svg" />
+      <hr />
+      <img id="philly-logo" src="./assets/city-of-philadelphia.svg" />
+    </div>
+    <div id="application-title">
+      <h2>Philadelphia Trip Generation</h2>
+    </div>
+  </header>
   <div id="layout">
     <div id="sidebar"><SideBar></SideBar></div>
     <Map v-model="location"></Map>
@@ -23,15 +33,16 @@ const location = {
   flex: 1;
   display: flex;
   width: 100%;
+  height: 100%;
 }
 
 #sidebar {
   color: black;
   background-color: white;
-  width: 25vw;
+  width: 28vw;
   max-height: 100vh;
-  padding-left: 1rem;
-  padding-right: 1rem;
+  padding-left: 2rem;
+  padding-right: 2rem;
   padding-top: 1rem;
   padding-bottom: 1rem;
   overflow-y: scroll;
@@ -39,5 +50,40 @@ const location = {
 
 .map-container {
   flex: 1;
+}
+
+#branding {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background-color: #2176d2;
+  height: 6vh;
+  width: 100%;
+  padding-right: 2rem;
+  padding-left: 2rem;
+}
+
+#application-title {
+  color: white;
+  display: inline-block;
+  background-color: #0f4d90;
+  width: 100%;
+  padding-right: 2rem;
+  padding-left: 2rem;
+}
+
+#dvrpc-logo {
+  height: 2rem;
+}
+
+#philly-logo {
+  height: 4rem;
+}
+
+#branding > hr {
+  height: 60%;
+  margin-left: 1rem;
+  margin-right: 0.4rem;
+  color: white;
 }
 </style>
